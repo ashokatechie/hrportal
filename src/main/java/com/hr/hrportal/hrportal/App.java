6c9483b1d294b3657203404a890a4d2e40ff312e
Ashok <ashokatechie@gmail.com>
2017-04-27 07:18:47 +0530

    test

---
+++ src/main/java/com/hr/hrportal/hrportal/App.java
@@ -0,0 +1,13 @@
+package com.hr.hrportal.hrportal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
