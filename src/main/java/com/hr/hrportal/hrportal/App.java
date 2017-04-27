8e937d96f358bc323811d21e97c996c2a17a3bc7
Limbrit <limbritjava@gmail.com>
2017-04-26 22:01:33 -0400

    Second commit

--- src/main/java/com/hr/hrportal/hrportal/App.java
+++ src/main/java/com/hr/hrportal/hrportal/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! !!! Test Test - Limbrit" );
+        System.out.println( "Hello World! !!! Test Test - Limbrit" );
         
     }
 }
